16216e4e37194774289081c75d724ca8eb23a5b8
kobakichi <fenderstrat57vin@gmail.com>
2022-07-21 04:58:52 +0900

    Home画面作成とrouterの設定完了

---
+++ front/React/src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+export const Home = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Typing EQ short_cut_key version</h1>
+      <p>ログインできましたよ〜〜〜！</p>
+    </div>
+  );
+};
