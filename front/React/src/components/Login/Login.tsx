0baa66bd91cefc1c9773ef2439836eb297816ef3
kobakichi <fenderstrat57vin@gmail.com>
2022-07-26 04:56:44 +0900

    Login.tsxとRegister.tsxの内容を修正して、動作確認。

--- front/React/src/components/Login/Login.tsx
+++ front/React/src/components/Login/Login.tsx
@@ -7,48 +7,48 @@ import styles from "./Login.module.css";
  * カスタムフック
  */
 import { useFormInput } from "../../hooks/useFormInput";
-import axios from "axios";
-import { RegisterUser } from "../../types/User";
-import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import { RegisterUser } from "../../types/User";
+// import { useNavigate } from "react-router-dom";
 
 export const Login: VFC = memo(() => {
   // hooksを分割代入する
   const [states, actions] = useFormInput();
-  const [errorMessage, setErrorMessage] = useState("");
-  const [isError, setIsError] = useState(false);
+  // const [errorMessage, setErrorMessage] = useState("");
+  // const [isError, setIsError] = useState(false);
 
-  const navigate = useNavigate();
-  // /loginエンドポイントへデータを送信する処理
-  const postLoginUser = async () => {
-    try {
-      const result = await axios.post<RegisterUser>(
-        "http://localhost:5001/api/login",
-        {
-          username: states.userName,
-          password: states.passWord,
-        }
-      );
-      return result.data;
-    } catch (err: any) {
-      throw new Error(err);
-    }
-  };
+  // const navigate = useNavigate();
+  // // /loginエンドポイントへデータを送信する処理
+  // const postLoginUser = async () => {
+  //   try {
+  //     const result = await axios.post<RegisterUser>(
+  //       "http://localhost:5001/api/login",
+  //       {
+  //         username: states.userName,
+  //         password: states.passWord,
+  //       }
+  //     );
+  //     return result.data;
+  //   } catch (err: any) {
+  //     throw new Error(err);
+  //   }
+  // };
 
-  // クリックした時に発火する関数
-  const onClickLogin = () => {
-    postLoginUser()
-      .then((result) => {
-        console.log(result);
-        // localstrageへtokenをセット
-        localStorage.setItem("token", result.access_token);
-        navigate("/");
-      })
-      .catch((err) => {
-        console.log(err);
-        setIsError(true);
-        setErrorMessage("入力内容をお確かめください。");
-      });
-  };
+  // // クリックした時に発火する関数
+  // const onClickLogin = () => {
+  //   postLoginUser()
+  //     .then((result) => {
+  //       console.log(result);
+  //       // localstrageへtokenをセット
+  //       localStorage.setItem("token", result.access_token);
+  //       navigate("/");
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       setIsError(true);
+  //       setErrorMessage("入力内容をお確かめください。");
+  //     });
+  // };
 
   return (
     <div className={styles.wrapper}>
@@ -84,7 +84,7 @@ export const Login: VFC = memo(() => {
                 borderColor={"#268BD2"}
                 color={"white"}
                 _hover={{ background: "#36DBCE", cursor: "pointer" }}
-                onClick={onClickLogin}
+                onClick={actions.onClickLogin}
               >
                 ログイン
               </Button>
@@ -92,7 +92,9 @@ export const Login: VFC = memo(() => {
           </Stack>
         </div>
         {/* ログインに失敗した時の処理 */}
-        {isError && <p className={styles.errorMessage}>{errorMessage}</p>}
+        {states.isError && (
+          <p className={styles.errorMessage}>{states.errorMessage}</p>
+        )}
       </div>
     </div>
   );
--- front/React/src/components/Register/Register.tsx
+++ front/React/src/components/Register/Register.tsx
@@ -1,68 +1,71 @@
 import { Button, Input, Stack } from "@chakra-ui/react";
 import React, { memo, VFC, useState, ChangeEvent } from "react";
-import { useNavigate } from "react-router-dom";
+import { useFormInput } from "../../hooks/useFormInput";
+// import { useNavigate } from "react-router-dom";
 import styles from "./Register.module.css";
 import { SubTitle } from "./SubTitle/SubTitle";
 import { Title } from "./Title/Title";
-import axios from "axios";
-import { RegisterUser } from "../../types/User";
+// import axios from "axios";
+// import { RegisterUser } from "../../types/User";
 
 export const Register: VFC = memo(() => {
-  const navigate = useNavigate();
+  // カスタムフックの呼び出し
+  const [states, actions] = useFormInput();
+  // const navigate = useNavigate();
 
-  const [userName, setUserName] = useState("");
-  const [passWord, setPassWord] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [isError, setIsError] = useState(false);
+  // const [userName, setUserName] = useState("");
+  // const [passWord, setPassWord] = useState("");
+  // const [errorMessage, setErrorMessage] = useState("");
+  // const [isError, setIsError] = useState(false);
 
   // usernameを保持する関数
-  const onChangeUserNameInput = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setUserName(e.target.value);
-  };
+  // const onChangeUserNameInput = (e: ChangeEvent<HTMLInputElement>) => {
+  //   e.preventDefault();
+  //   setUserName(e.target.value);
+  // };
 
-  // passwordを保持する関数
-  const onChangePasswordInput = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setPassWord(e.target.value);
-  };
+  // // passwordを保持する関数
+  // const onChangePasswordInput = (e: ChangeEvent<HTMLInputElement>) => {
+  //   e.preventDefault();
+  //   setPassWord(e.target.value);
+  // };
 
-  // ユーザーの登録処理
-  const postRegisterUser = async () => {
-    try {
-      const result = await axios.post<RegisterUser>(
-        "http://localhost:5001/api/register",
-        {
-          username: userName,
-          password: passWord,
-        }
-      );
-      return result.data;
-    } catch (err: any) {
-      // if (err.response.status === 401) {
-      //   setErrorMessage(`${userName}は既に存在しています。`);
-      // }
-      throw new Error(err);
-    }
-  };
+  // // ユーザーの登録処理
+  // const postRegisterUser = async () => {
+  //   try {
+  //     const result = await axios.post<RegisterUser>(
+  //       "http://localhost:5001/api/register",
+  //       {
+  //         username: userName,
+  //         password: passWord,
+  //       }
+  //     );
+  //     return result.data;
+  //   } catch (err: any) {
+  //     // if (err.response.status === 401) {
+  //     //   setErrorMessage(`${userName}は既に存在しています。`);
+  //     // }
+  //     throw new Error(err);
+  //   }
+  // };
 
-  // 登録ボタンを押した時に発火する関数
-  const onClickRegister = () => {
-    postRegisterUser()
-      .then((result) => {
-        console.log(result);
-        // localstrageへaccess_tokenをセットする
-        localStorage.setItem("token", result.access_token);
-        // 登録が完了したらhomeへ移動する。
-        navigate("/");
-      })
-      .catch((err) => {
-        console.log(err);
-        setIsError(true);
-        setErrorMessage(`${userName}は既に存在しています。`);
-      });
-    // .finally(() => console.log("登録完了"));
-  };
+  // // 登録ボタンを押した時に発火する関数
+  // const onClickRegister = () => {
+  //   postRegisterUser()
+  //     .then((result) => {
+  //       console.log(result);
+  //       // localstrageへaccess_tokenをセットする
+  //       localStorage.setItem("token", result.access_token);
+  //       // 登録が完了したらhomeへ移動する。
+  //       navigate("/");
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       setIsError(true);
+  //       setErrorMessage(`${userName}は既に存在しています。`);
+  //     });
+  //   // .finally(() => console.log("登録完了"));
+  // };
 
   return (
     <div className={styles.wrapper}>
@@ -78,7 +81,7 @@ export const Register: VFC = memo(() => {
               borderColor={"#268BD2"}
               placeholder="user name"
               _placeholder={{ opacity: 1, color: "#268BD2" }}
-              onChange={onChangeUserNameInput}
+              onChange={actions.onChangeUserNameInput}
             />
             <Input
               type="password"
@@ -88,7 +91,7 @@ export const Register: VFC = memo(() => {
               borderColor={"#268BD2"}
               placeholder="pass word"
               _placeholder={{ opacity: 1, color: "rgba(38, 139, 210, 1)" }}
-              onChange={onChangePasswordInput}
+              onChange={actions.onChangePasswordInput}
             />
             <div className={styles.button}>
               <Button
@@ -98,14 +101,17 @@ export const Register: VFC = memo(() => {
                 borderColor={"#268BD2"}
                 color={"white"}
                 _hover={{ background: "#36DBCE", cursor: "pointer" }}
-                onClick={onClickRegister}
+                onClick={actions.onClickRegister}
               >
                 登録する
               </Button>
             </div>
           </Stack>
         </div>
-        {isError && <p className={styles.errorMessage}>{errorMessage}</p>}
+        {/* ユーザーが重複していた場合の処理 */}
+        {states.isError && (
+          <p className={styles.errorMessage}>{states.errorMessage}</p>
+        )}
       </div>
     </div>
   );
