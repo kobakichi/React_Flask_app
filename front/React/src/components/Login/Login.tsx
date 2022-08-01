5969396da10ff701b209f41b70c4a657836063ca
kobakichi <fenderstrat57vin@gmail.com>
2022-08-02 06:01:11 +0900

    example

--- front/React/src/components/Login/Login.tsx
+++ front/React/src/components/Login/Login.tsx
@@ -9,16 +9,17 @@ import React, { memo, VFC } from "react";
 import { SubTitle } from "../Register/SubTitle/SubTitle";
 import { Title } from "../Register/Title/Title";
 import styles from "./Login.module.css";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 /**
  * カスタムフック
  */
 import { useFormInput } from "../../hooks/useFormInput";
+import { RegisterUser } from "../../types/User";
 
-type IFormInput = {
-  userName: string;
-  passWord: string;
-};
+// type RegisterUser = {
+//   userName: string;
+//   passWord: string;
+// };
 
 export const Login: VFC = memo(() => {
   // hooksを分割代入する
@@ -27,8 +28,17 @@ export const Login: VFC = memo(() => {
   // react-hook-formのバリデーション
   const {
     register,
+    handleSubmit,
+    watch,
     formState: { errors },
-  } = useForm({ mode: "all" });
+  } = useForm<RegisterUser>({ mode: "all" });
+
+  // formタグに渡す関数
+  const handleOnSubmit: SubmitHandler<RegisterUser> = (values) => {
+    console.log(values);
+  };
+
+  console.log(watch("username"));
 
   return (
     <div className={styles.wrapper}>
@@ -36,56 +46,58 @@ export const Login: VFC = memo(() => {
         <Title text="Typing EQ" />
         <SubTitle text="short-cut-key version" />
         <div className={styles.form}>
-          <Stack spacing={10}>
-            <FormControl
-              id="inputUser"
-              isRequired
-              isInvalid={errors.inputUser ? true : false}
-            >
+          <form onSubmit={handleSubmit(handleOnSubmit)}>
+            <Stack spacing={10}>
+              {/* usernameのフォームコントロール */}
+              <FormControl
+                id="username"
+                isRequired
+                isInvalid={errors.username ? true : false}
+              >
+                <Input
+                  color={"white"}
+                  size={"lg"}
+                  placeholder="user name"
+                  background={"#0A5163"}
+                  borderColor={"#268BD2"}
+                  _placeholder={{ opacity: 1, color: "#268BD2" }}
+                  {...register("username", {
+                    required: "ユーザー名を入力してください",
+                    maxLength: {
+                      value: 10,
+                      message: "10文字以内で入力してください",
+                    },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.username && errors.username.message}
+                </FormErrorMessage>
+              </FormControl>
               <Input
+                type="password"
                 color={"white"}
                 size={"lg"}
-                placeholder="user name"
                 background={"#0A5163"}
                 borderColor={"#268BD2"}
-                _placeholder={{ opacity: 1, color: "#268BD2" }}
-                {...register("inputUser", {
-                  required: "ユーザー名を入力してください",
-                  maxLength: {
-                    value: 10,
-                    message: "10文字以内で入力してください",
-                  },
-                })}
-                onChange={actions.onChangeUserNameInput}
+                placeholder="pass word"
+                _placeholder={{ opacity: 1, color: "rgba(38, 139, 210, 1)" }}
+                onChange={actions.onChangePasswordInput}
               />
-              <FormErrorMessage>
-                {errors.inputUser && errors.inputUser.message}
-              </FormErrorMessage>
-            </FormControl>
-            <Input
-              type="password"
-              color={"white"}
-              size={"lg"}
-              background={"#0A5163"}
-              borderColor={"#268BD2"}
-              placeholder="pass word"
-              _placeholder={{ opacity: 1, color: "rgba(38, 139, 210, 1)" }}
-              onChange={actions.onChangePasswordInput}
-            />
-            <div className={styles.button}>
-              <Button
-                size={"lg"}
-                width="200px"
-                background={"#2AA198"}
-                borderColor={"#268BD2"}
-                color={"white"}
-                _hover={{ background: "#36DBCE", cursor: "pointer" }}
-                onClick={actions.onClickLogin}
-              >
-                ログイン
-              </Button>
-            </div>
-          </Stack>
+              <div className={styles.button}>
+                <Button
+                  size={"lg"}
+                  width="200px"
+                  background={"#2AA198"}
+                  borderColor={"#268BD2"}
+                  color={"white"}
+                  _hover={{ background: "#36DBCE", cursor: "pointer" }}
+                  onClick={actions.onClickLogin}
+                >
+                  ログイン
+                </Button>
+              </div>
+            </Stack>
+          </form>
         </div>
         {/* ログインに失敗した時の処理 */}
         {states.isError && (
